7c17cb9696bcc1229a3a86c4b1c790351dbc1e10
Kishan0910 <mishrakishan240@gmail.com>
2025-08-21 04:24:41 +0000

    ConversionOperations

---
+++ 03_conversionOperations.js
@@ -0,0 +1,15 @@
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Kishan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
