00a70803bd2d145290b8839e5e45b8ec328016d6
Kishan0910 <mishrakishan240@gmail.com>
2025-08-21 06:29:55 +0000

    Comparison

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -11,5 +11,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ******* Operations ********//
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(4%2)
+
+let str1 = "hello"
+let str2 = " Kishan"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);  // postfix and prefix js mkd (website)
---
+++ 04_comparison.js
@@ -0,0 +1,36 @@
+
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+
+//  console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);       
+// console.log(null == 0);
+// console.log(null >= 0);
+
+/* The reason behind is that an equality check == and comparisons ><>=<= work differently.
+   Comparisons convert null to a number, treating it as 0.
+   That's why (3) null >=0 is true and (1) null = 0 is false.
+*/   
+
+// console.log(true > 0);
+// console.log(false > 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// console.log("Kishan" > 0);
+// console.log("" > 0);
+// console.log("" == 0);
+// console.log("Hii" == 1);
+
+// === 
+
+console.log("2" === 2);
