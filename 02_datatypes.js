26f6bc58b44d623f506ac8b145aa913c4ea586ea
Kishan0910 <mishrakishan240@gmail.com>
2025-08-21 04:18:35 +0000

    DataTypes

---
+++ 02_datatypes.js
@@ -0,0 +1,27 @@
+"use strick;"
+const { log } = require("console")
+
+ //treat all JS code as newer version!
+
+// alert( 3 + 4) // we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("Kishan")
+
+let name = "Kishan"
+let age = 18
+let isLOggedIn = false
+let state;
+
+// number => 2 to power of 53
+// bignit
+//string = ""
+// boolean => true/false
+// null => standalone value
+// undefine => value not define
+// symbol => unique
+
+// object
+
+console.log(typeof undefined )  // undefined
+console.log(typeof null);       // object
