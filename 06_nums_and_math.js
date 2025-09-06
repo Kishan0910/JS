26c7738dd985766480c0787148f895df7a3a9ba0
Kishan0910 <mishrakishan240@gmail.com>
2025-09-06 16:35:09 +0000

    Numbers and Maths

---
+++ 06_nums_and_math.js
@@ -0,0 +1,34 @@
+const score = 400
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 28.49569
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hund = 1000000
+// console.log(hund.toLocaleString('en-IN'));
+
+// ************ Maths ***************** //
+
+// console.log(Math);
+// console.log(Math.abs(-4));     // It chane the neative value into positive value
+// console.log(Math.round(4.9));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.sqrt(4));
+// console.log(Math.min(2, 3, 1.2));
+// console.log(Math.max(2, 3, 1.2));
+
+console.log(Math.random());  //The value of random in between 0 & 1
+console.log((Math.random()*10 )+ 1); 
+
+const min = 10
+const max  = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
